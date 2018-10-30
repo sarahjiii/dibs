73f45908b69e037263d8efceba41198b88daf0f4
yasminenassar <ynassar@ucsd.edu>
2018-10-30 15:53:09 -0700

    adding food preferences

--- js/introHCI.js
+++ js/introHCI.js
@@ -1,5 +1,6 @@
 // Call this function when the page loads (the jQuery "ready" event)
 $(document).ready(function() {
+  $('#body').show();
 	initializePage();
 })
 
@@ -7,10 +8,6 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
-
 	$( "nav" ).hide();
 	$( ".hamburger" ).click(function() {
 		$( "nav" ).slideToggle( "slow", function() {});
