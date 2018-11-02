f17f5b0b4b4ce3bcbcce6a97b5ab9cb05dd04711
sarahji <s9ji@ucsd.edu>
2018-11-02 00:18:50 -0700

    got bored, added camera clicking functionality on posting page

--- js/introHCI.js
+++ js/introHCI.js
@@ -13,3 +13,26 @@ function initializePage() {
 		$( "nav" ).slideToggle( "slow", function() {});
 	});
 }
+
+/*$(function () {
+  $("#camfile").click(function () {
+    $("#uploadfile").click();
+  });
+  $("#uploadfile").change(function() {
+    var fileName = $(this).val().split('\\')[$(this).val().split('\\').length - 1];
+    filePath.html("<b>Selected File: </b>" + fileName);
+  });
+});*/
+
+$(function () {
+    var fileupload = $("#FileUpload1");
+    var filePath = $("#spnFilePath");
+    var button = $("#btnFileUpload");
+    button.click(function () {
+        fileupload.click();
+    });
+    fileupload.change(function () {
+        var fileName = $(this).val().split('\\')[$(this).val().split('\\').length - 1];
+        filePath.html("<b>Selected File: </b>" + fileName);
+    });
+});
