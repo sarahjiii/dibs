fc7f97f648200c8c995ca5af622413b490046a05
yasminenassar <ynassar@ucsd.edu>
2018-11-05 18:09:49 -0800

    adding js functions

--- js/introHCI.js
+++ js/introHCI.js
@@ -1,3 +1,4 @@
+var postIndex = 0;
 // Call this function when the page loads (the jQuery "ready" event)
 $(document).ready(function() {
   $('#header').show();
@@ -14,6 +15,40 @@ function initializePage() {
 	});
 }
 
+function postClick() {
+  var foodItem = document.getElementById("foodItem").value;
+  var loc = document.getElementById("location").value;
+  var time = document.getElementById("time").value;
+  var contains = document.getElementsByName("contains");
+  var containsStr = "Contains: ";
+  for (var i = 0; i < contains.length; i++) {
+    if (contains[i].checked) {
+      containsStr = containsStr + contains[i].defaultValue + " ";
+    }
+  }
+
+  var postObject = {'foodItem': foodItem,
+                  'location': loc,
+                  'time': time,
+                   'contains': containsStr};
+  var postName = "post" + postIndex;
+  localStorage.setItem(postName, JSON.stringify(postObject));
+  postIndex++;
+  console.log(postObject);
+}
+
+function displayPosts(){
+  var source   = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  var parentDiv = $("#templatedPosts");
+
+  for(var i = 0; i < postIndex; i++){
+    var postId = "post" + i;
+    var curObject = JSON.parse(localStorage.getItem(postId));
+    var curHtml = template(curObject);
+    parentDiv.append(curHtml);
+  }
+}
 /*$(function () {
   $("#camfile").click(function () {
     $("#uploadfile").click();
