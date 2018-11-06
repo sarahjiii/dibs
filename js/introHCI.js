de5b34053ccd545a3047030e98d96858ff8d0cf2
yasminenassar <ynassar@ucsd.edu>
2018-11-05 19:10:55 -0800

    can add posts to main page

--- js/introHCI.js
+++ js/introHCI.js
@@ -1,8 +1,9 @@
-var postIndex = 0;
+//var postIndex = 0;
 // Call this function when the page loads (the jQuery "ready" event)
 $(document).ready(function() {
   $('#header').show();
 	initializePage();
+  displayPosts();
 })
 
 /*
@@ -20,7 +21,7 @@ function postClick() {
   var loc = document.getElementById("location").value;
   var time = document.getElementById("time").value;
   var contains = document.getElementsByName("contains");
-  var containsStr = "Contains: ";
+  var containsStr = "";
   for (var i = 0; i < contains.length; i++) {
     if (contains[i].checked) {
       containsStr = containsStr + contains[i].defaultValue + " ";
@@ -28,25 +29,49 @@ function postClick() {
   }
 
   var postObject = {'foodItem': foodItem,
-                  'location': loc,
+                  'loc': loc,
                   'time': time,
                    'contains': containsStr};
+  var postIndex = 0;
+
+  if(localStorage.getItem("postIndex") != null){
+    postIndex = parseInt(localStorage.getItem("postIndex"));
+  }
   var postName = "post" + postIndex;
   localStorage.setItem(postName, JSON.stringify(postObject));
-  postIndex++;
+  postIndex = postIndex + 1;
+  localStorage.removeItem("postIndex");
+  localStorage.setItem("postIndex", postIndex);
   console.log(postObject);
 }
 
 function displayPosts(){
-  var source   = $("#entry-template").html();
+  var source = $("#entry-template").html();
+  console.log("here she is");
+  console.log(source);
   var template = Handlebars.compile(source);
   var parentDiv = $("#templatedPosts");
+  //get the number of posts made so far
+  var postIndex;
 
+  if(localStorage.getItem("postIndex") == null){
+    postIndex = 0;
+  }
+  else{
+    postIndex = localStorage.getItem("postIndex");
+  }
+  
+  console.log("postIndex: ", postIndex);
+  //clear the parentDiv to make sure we're not appending over and over again
+  parentDiv.html("");
   for(var i = 0; i < postIndex; i++){
     var postId = "post" + i;
     var curObject = JSON.parse(localStorage.getItem(postId));
+    console.log("curObject");
+    console.log(curObject);
     var curHtml = template(curObject);
     parentDiv.append(curHtml);
+
   }
 }
 /*$(function () {
